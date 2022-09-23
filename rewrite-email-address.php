0419d7175286f3cad290f342fc555202d23d6e0b
Tahina R <to.tahina@gmail.com>
2022-09-23 15:30:24 +0300

    Updated version in main file

--- rewrite-email-address.php
+++ rewrite-email-address.php
@@ -3,7 +3,7 @@
  * Plugin Name: Rewrite Email Address
  * Plugin URI: https://wordpress.org/rtahina/rewrite-email-address/
  * Description: This plugin rewrites your email address so that web crawler  won't be able to harvest them. Ex: myemail@adress.com would be rewritten to myemail_At_address.com
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Tahina Rakotomanarivo.
  * Author URI: https://github.com/rtahina
  * License: GPL2+
